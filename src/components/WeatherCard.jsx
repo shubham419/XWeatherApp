c5a5e286fc7c5c4f02533fecbe6793f14857063f
shubham <thoratshubham161@gmail.com>
2024-02-15 16:56:42 +0530

    class name added

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,7 +3,7 @@ const WeatherCard = ({title, value}) => {
   return (
     <div className="weather-card">
         <h3>{title}</h3>
-        <p>{value}</p>
+        <h4>{value}</h4>
     </div>
   )
 }
